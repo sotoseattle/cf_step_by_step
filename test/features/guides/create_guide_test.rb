669b08317871cbda0bb647fcbd1b949e263e1e4a
Javier Soto <sotoseattle@gmail.com>
2014-09-30 19:27:16 -0700

    add test for creating guides

---
+++ test/features/guides/create_guide_test.rb
@@ -0,0 +1,28 @@
+require './test/test_helper'
+
+feature 'Create a new guide' do
+  scenario 'happy path' do
+    visit guidess_path
+    click_on 'Create New Guide'
+
+    fill_in 'Name', with: 'How to Install a Toilet'
+    click_on 'Submit'
+
+    page.text.must_include 'Guide was successfully created'
+    page.text.must_include 'How to Install a Toilet'
+    page.status_code.must_equal 200
+  end
+
+  scenario 'fail to submit form to create a new guide' do
+    visit guides_path
+    click_on 'Create New Guide'
+
+    initial_guides_count = Guide.count
+    fill_in 'Name', with: 'Q'
+    click_on 'Submit'
+
+    Guide.count.must_equal initial_guides_count
+    page.text.must_include 'errors prohibited this guide from being saved'
+    page.text.must_include 'Name is too short'
+  end
+end
